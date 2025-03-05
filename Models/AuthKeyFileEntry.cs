f66334a302a9bd51300201c82f7197876db42d8b
firealarmss <caleb.k4php@gmail.com>
2025-03-05 11:05:34 -0600

    Update reloader

---
+++ Models/AuthKeyFileEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhackerLinkLib.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AuthKeyFileEntry
+    {
+        public bool Enabled { get; set; }
+        public string Alias { get; set; }
+        public string AuthKey { get; set; }
+    }
+}
